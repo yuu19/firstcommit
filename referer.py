f99ae6f6de47cfdebbc3c583d4ed490a195563cb
yuu19 <yusuke.kusi1028@gmail.com>
2020-05-04 21:04:58 +0900

    refererヘッダーを追加する

---
+++ referer.py
@@ -0,0 +1,13 @@
+#Refererヘッダー追加
+import requests
+
+url = 'http://www.webscrapingfordatascience.com/referercheck/secret.php'
+
+my_headers = {
+        'Referer': 'http://www.webscrapingfordatascience.com/referercheck/'
+        }
+
+r = requests.get(url, headers=my_headers)
+print(r.text)
+
+
