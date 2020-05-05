f7e39c7f0f94057ccaf2195a4d3346d8bc655242
yuu19 <yusuke.kusi1028@gmail.com>
2020-05-05 21:49:34 +0900

    hai

---
+++ 4th_example.py
@@ -0,0 +1,34 @@
+import sys
+import requests
+from bs4 import BeautifulSoup
+print(sys.version_info)
+url = 'https://en.wikipedia.org/wiki/List_of_Game_of_Thrones_episodes'
+
+r = requests.get(url)
+html_contents = r.text
+html_soup = BeautifulSoup(html_contents, 'html.parser')
+
+#リストを用いて一覧を保存する
+episodes = []
+
+ep_tables = html_soup.find_all('table', class_='wikitable plainrowheaders')
+
+for table in ep_tables:
+    headers = []
+    rows = table.find_all('tr')
+
+    for header in table.find('tr').find('th'):
+        headers.append(header.text)
+
+    for row in table.find_all('tr')[1:]:
+        values = []
+        for col in row.find_all(['th', 'td']):
+            values.append(col.text)
+        if values:
+            episode_dict = {headers[i]: values[i] for i in  range(len(values))}
+            eposodes.append(episode_dict)
+
+
+for episode in episodes:
+    print(episode)
+    
--- firstexample.py
+++ firstexample.py
@@ -1,11 +1,12 @@
 import requests
 url = 'https://www.youtube.com/watch?v=KMZF4gXc8OQ&t=1397s'
 r = requests.get(url)
-print(r.status_code)
-print(r.reason)
-print(r.headers)
-print(r.request)
-print(r.request.headers)
-print(r.request.url)
+print(r.status_code) #HTTPステータスコード
+print(r.reason) #ステータスメッセージ　
+print(r.headers) #ヘッダー
+print(r.request) #リクエスト情報
+print(r.request.headers) #HTTPリクエストヘッダー
+print(r.text) #コンテンツ
+
 
 
